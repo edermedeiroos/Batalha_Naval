aa00fc123df74e1b3c86dee9f9765ee49f0a1529
Madeira <ederlissemedeiros@gmail.com>
2025-06-18 11:48:20 -0300

    Final Commit
    Jogo pronto.

--- Batalha_Naval.py
+++ Batalha_Naval.py
@@ -8,7 +8,7 @@ def criar_tabuleiro(tamanho, navios=''):
         orientacao = random.choice(['HOR', 'VER']) # Boat orientation random choice
 
         if orientacao == 'VER':
-            y = random.randint(0, 10 - navio['Celulas']) # Boat inicial y coordinate 
+            y = random.randint(0, 10 - navio['Celulas']) # Boat inicial y coordinate
             x = random.randint(0, 9) # Boat inicial x coordinate
 
             while not verifica_posicoes(tabuleiro, y, x, navio['Celulas'], orientacao): # While any rand position is filled remake the coordinates
@@ -16,18 +16,18 @@ def criar_tabuleiro(tamanho, navios=''):
                 x = random.randint(0, 9)
 
             for i in range(navio['Celulas']): # Executed when (y, x) coordinates finded are not filled
-                tabuleiro[y + i][x] = f' {letra_navio} ' 
+                tabuleiro[y + i][x] = f' {letra_navio} '
 
         else:
-            x = random.randint(0, 10 - navio['Celulas']) # Boat inicial y coordinate 
-            y = random.randint(0, 9) # Boat inicial x coordinate 
+            x = random.randint(0, 10 - navio['Celulas']) # Boat inicial y coordinate
+            y = random.randint(0, 9) # Boat inicial x coordinate
 
             while not verifica_posicoes(tabuleiro, y, x, navio['Celulas'], orientacao): # While any rand position is filled remake the coordinates
                 x = random.randint(0, 10 - navio['Celulas'])
                 y = random.randint(0, 9)
 
             for i in range(navio['Celulas']):
-                tabuleiro[y][x + i] = f' {letra_navio} ' 
+                tabuleiro[y][x + i] = f' {letra_navio} '
 
     return tabuleiro
 
@@ -47,28 +47,46 @@ def verifica_posicoes(mat, y, x, quant, orient):
                 verificado = False
                 break
             verificado = True # If not filled, the verification became True
-    
+
     return verificado
 
-def visualizacao(tabuleiro): # Board Visualization
+def visualizacao(tabuleiro, esconder = True): # Board Visualization
+    def formatacao(string, esconder_nome = True):
+        if esconder_nome:
+            print(string.replace("'", "").replace(",", "").replace("P", "O").replace("N", "O").replace("C", "O").replace("D", "O").replace("S", "O"))
+        else:
+            print(string.replace("'", "").replace(",", ""))
+
     print('     ', end='')
-    for i in range(len(tabuleiro)): # Letters 
+    for i in range(len(tabuleiro)): # Letters
         print(chr(65+i), end='   ')
     print()
-
     for indice, linha in enumerate(tabuleiro): # Numbers
         if indice + 1 < 10:
-            print(f'0{indice + 1} {linha}'.replace("'", "").replace(',', '')) 
+            formatacao(f'0{indice + 1} {linha}', esconder_nome=esconder)
         else:
-            print(f'{indice + 1} {linha}'.replace("'", "").replace(',', ''))
+            formatacao(f'{indice + 1} {linha}', esconder_nome=esconder)
+
+def contador_barcos(tabuleiro, navios):
+    navios_destruidos = 0 
+    set_navios = navios.copy() 
+
+    for navio in set_navios: # Remove repeted boat's
+        if set_navios.count(navio) > 1:
+            set_navios.remove(navio)
+
+    for navio in set_navios: # Counter += 1 if tabuleiro.count letter is a Boat's size multiple
+        navios_destruidos += (str(tabuleiro).count(navio["Navio"][0]))//(navio["Celulas"])
+
+    return(navios_destruidos)
 
 navios = [{'Navio': 'Porta-Avioes', 'Celulas': 5}, # Boats name and size
           {'Navio': 'Navio-de-Batalha', 'Celulas': 4},
           {'Navio': 'Cruzador', 'Celulas': 3},
-          {'Navio': 'Destruidor1', 'Celulas': 2},
-          {'Navio': 'Destruidor2', 'Celulas': 2},
-          {'Navio': 'Submarino1', 'Celulas': 1},
-          {'Navio': 'Submarino2', 'Celulas': 1}]
+          {'Navio': 'Destruidor', 'Celulas': 2},
+          {'Navio': 'Destruidor', 'Celulas': 2},
+          {'Navio': 'Submarino', 'Celulas': 1},
+          {'Navio': 'Submarino', 'Celulas': 1}]
 letras_navios = [f' {navio["Navio"][0]} ' for navio in navios] # Boats inicial letter
 
 tabuleiro_verdade = criar_tabuleiro(10, navios) # Board with boats
@@ -92,41 +110,44 @@ while True: # Difficult mode choice
         print("*Modo de jogo desconhecido*", end='\n\n')
 
 for i in range(1, tentativas + 1): # Game
-    visualizacao(tabuleiro_jogador)
-    visualizacao(tabuleiro_verdade)
-    print(' '*50, f'JOGADAS RESTANTES: {tentativas + 1 - i}')
+    visualizacao(tabuleiro_jogador, esconder=True)
+    # visualizacao(tabuleiro_verdade, esconder=False)
+    print(' '*10, f'| JOGADAS RESTANTES: {tentativas + 1 - i} |')
+    print(' '*10, f'| BARCOS DESTRUIDOS: {contador_barcos(tabuleiro_jogador, navios)}  |')
+    print()
 
-    while True: # Error verification
+    while True: # Player's move and Error verification
         try:
-            jogada = str(input("Coordenada do Tiro [Coluna/Linha]: ")).upper() # Player's move
+            jogada = str(input("- Coordenada do Tiro [Coluna/Linha]: ")).upper() # Player's move
             linha = int(jogada[1:]) - 1 # Board Line
             coluna = int(ord(jogada[0]) - ord("A")) # Board Column
 
             if linha not in range(len(tabuleiro_verdade)) or coluna not in range(len(tabuleiro_verdade)):
-                print("*TIRO FORA DOS LIMITES*") # Shot out of the range
+                print("*TIRO FORA DOS LIMITES*", end='\n\n') # Shot out of the range
                 continue
 
-            elif tabuleiro_jogador[linha][coluna] == ' X ':
-                print("*TIRO REPETIDO*") # Repeated Shot
+            elif tabuleiro_jogador[linha][coluna] == ' X ' or tabuleiro_jogador[linha][coluna] in letras_navios:
+                print("*TIRO REPETIDO*", end='\n\n') # Repeated Shot
                 continue
 
         except (ValueError, TypeError):
-            print("*JOGADA INVÁLIDA*") # Invalid move (Usually switch between line and column)
+            print("*JOGADA INVÁLIDA*", end='\n\n') # Invalid move (Usually switch between line and column)
             continue
-        
+
         break
 
     if tabuleiro_verdade[linha][coluna] in letras_navios: # Shot verification
-        print("ACERTOU!")
-        tabuleiro_jogador[linha][coluna] = ' O ' # Player board update
+        print("ACERTOU!", end='\n\n')
+        tabuleiro_jogador[linha][coluna] = tabuleiro_verdade[linha][coluna] # Player board update
     else:
-        print("ERROU!")
+        print("ERROU!", end='\n\n')
         tabuleiro_jogador[linha][coluna] = ' X ' # Player board update
 
-    # TODO refazer as letras do tabuleiro_jogador para verificar com tabuleiro_verdade
-    if tabuleiro_jogador == tabuleiro_verdade: # Win condition
+    if contador_barcos(tabuleiro_jogador, navios) == len(navios): # Win condition
         print(" - VOCÊ GANHOU! - ")
         break
 
-if tabuleiro_jogador != tabuleiro_verdade:
+if contador_barcos(tabuleiro_jogador, navios) != len(navios): # Defeat condition
     print(" - VOCÊ PERDEU! - ")
+    print(f'\n - GABARITO DOS BARCOS: \n')
+    visualizacao(tabuleiro_verdade, esconder=True)
